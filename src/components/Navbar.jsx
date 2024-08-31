dbe0c0c69351a8fb9c5f417f6563fdb37dedeb32
Nayan <nayan92001@gmail.com>
2024-09-01 03:02:11 +0530

    Refactored Navbar to use MUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,63 @@
+import { Box, Button, Typography } from '@mui/material';
 import React, { useContext } from 'react'
+
 import KanbanContext from "../contexts/KanbanContext";
+
 const Navbar = () => {
-    const {columns, items} = useContext(KanbanContext);
+    const { columns, items } = useContext(KanbanContext);
     return (
-        <div className='nav'>
-            <div>
-                <h1>Kanban Board</h1>
-            </div>
-            <div
-                style={{
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '1em',
+                backgroundColor: 'rgb(23, 39, 55)'
+            }}
+        >
+            <Box>
+                <Typography variant='h3' color='white'>Kanban Board</Typography>
+            </Box>
+            <Box
+                sx={{
                     display: 'flex',
                     gap: '1em'
                 }}
             >
-                <button
-                    className='btn'
+                <Button
+                    sx={{
+                        textTransform: 'none',
+                        backgroundColor: 'rgb(20, 77, 112)',
+                        color: 'white',
+                        fontWeight: 'bold',
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                        },
+                        transition: 'transform 0.1s ease-in-out'
+                    }}
                     onClick={() => {
                         localStorage.setItem('items', JSON.stringify({}));
                         localStorage.setItem('columns', JSON.stringify([]));
                     }}
-                >Clear State</button>
-                <button
-                    className='btn'
+                >Clear State</Button>
+                <Button
+                    sx={{
+                        textTransform: 'none',
+                        backgroundColor: 'rgb(20, 77, 112)',
+                        color: 'white',
+                        fontWeight: 'bold',
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                        },
+                        transition: 'transform 0.1s ease-in-out'
+                    }}
                     onClick={() => {
                         localStorage.setItem('items', JSON.stringify(items));
                         localStorage.setItem('columns', JSON.stringify(columns));
                     }}
-                >Save State</button>
-            </div>
-        </div>
+                >Save State</Button>
+            </Box>
+        </Box>
     )
 }
 
