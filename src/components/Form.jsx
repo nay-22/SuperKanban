0bc44a9ca8b5f5d0e2908a386bed192faea808f2
Nayan <nayan92001@gmail.com>
2024-09-03 10:55:01 +0530

    Bare minimum responsiveness | TODO: IMPROVE

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,20 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 
 import ColumnForm from './forms/ColumnForm';
 import TaskForm from './forms/TaskForm';
 
 const Form = () => {
+
+    const theme = useTheme();
+
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
     
     return (
         <Box
             sx={{
                 display: 'flex',
+                flexDirection: isSmallScreen ? 'column' : 'row',
                 alignItems: 'center',
                 justifyContent: 'center',
                 gap: '2em',
