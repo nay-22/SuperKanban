ad15c911c45408806921ce8fcc6541e0aa1316dd
Nayan <nayan92001@gmail.com>
2024-09-18 04:21:39 +0530

    Added Search Bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import { Add, Menu, ViewColumn } from '@mui/icons-material';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import { Add, Menu, Search, ViewColumn } from '@mui/icons-material';
+import { AppBar, Button, InputAdornment, TextField, Toolbar, Typography } from '@mui/material'
 import { useColumnActions } from '../hooks/ColumnActions';
 
 const Navbar = () => {
-    const {createColumn} = useColumnActions();
+    const { createColumn } = useColumnActions();
 
     const clearKanban = () => {
         localStorage.setItem('columns', JSON.stringify([]));
@@ -11,17 +11,52 @@ const Navbar = () => {
     }
 
     return (
-        <AppBar sx={{bgcolor: 'transparent'}} position='sticky'>
-            <Toolbar className='bg-mainBackgroundColor'>
+        <AppBar sx={{ bgcolor: 'transparent' }} position='sticky'>
+            <Toolbar className='bg-mainBackgroundColor' sx={{ gap: '1em', justifyContent: 'space-between' }}>
                 <Button>
                     <Menu />
                 </Button>
-                <Typography sx={{ flexGrow: 1 }} variant='h5'>
+                <Typography variant='h5'>
                     SuperKanban
                 </Typography>
+                <TextField
+                    size="small"
+                    placeholder="Search Projects, Boards..."
+                    sx={{
+                        width: '80%',
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                                borderColor: 'rgba(255, 255, 255, 0.3)',
+                            },
+                            '&:hover fieldset': {
+                                borderColor: 'white',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: 'white',
+                            },
+                            '&.Mui-focused': {
+                                backgroundColor: 'transparent',
+                            },
+                        },
+                        input: {
+                            color: 'white',
+                        },
+                    }}
+                    slotProps={{
+                        input: {
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <Search sx={{ color: 'white' }} />
+                                </InputAdornment>
+                            )
+                        }
+                    }}
+                />
+
+
                 <div className='flex items-center justify-between gap-5'>
-                    <Button onClick={clearKanban} variant='outlined' sx={{textTransform:'none', minWidth: '0px'}}>Clear</Button>
-                    <Button onClick={createColumn} variant='outlined' sx={{textTransform:'none', minWidth: '0px'}}><ViewColumn /><Add /></Button>
+                    <Button onClick={clearKanban} variant='outlined' sx={{ textTransform: 'none', minWidth: '0px' }}>Clear</Button>
+                    <Button onClick={createColumn} variant='outlined' sx={{ textTransform: 'none', minWidth: '0px' }}><ViewColumn /><Add /></Button>
                 </div>
             </Toolbar>
         </AppBar>
