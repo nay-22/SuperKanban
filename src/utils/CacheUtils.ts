e732357d523ec48f5500efdbc5d468305c508fdc
Nayan <nayan92001@gmail.com>
2024-09-19 12:48:21 +0530

    Clear cache for users as well

--- src/utils/CacheUtils.ts
+++ src/utils/CacheUtils.ts
@@ -1,7 +1,12 @@
+import { KBMember } from "../types";
+
 export const cacheItem = (key: string, value: any) => {
     localStorage.setItem(key, JSON.stringify(value));
 }
 
 export const clearCache = () => {
     localStorage.setItem('projects', JSON.stringify({}));
+    const users: KBMember[] = JSON.parse(localStorage.getItem('users')!);
+    users.map(user => user.projects = []);
+    localStorage.setItem('users', JSON.stringify(users));
 }
