b27aa88952f5265424052969dcc4d5408b1c58c7
Nayan <nayan92001@gmail.com>
2024-09-21 00:09:05 +0530

    Added Tooltip for better info on links and dynamically color current/last link

--- src/components/TrailLinks.tsx
+++ src/components/TrailLinks.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumbs, Typography } from '@mui/material'
+import { Breadcrumbs, Tooltip, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import KanbanContext from '../contexts/KanbanContext'
@@ -20,18 +20,57 @@ const TrailLinks: React.FC<TrailLinkProps> = ({ children }) => {
         <div className={`flex items-center justify-between py-4 px-12 'w-full`}>
             <div role="presentation">
                 <Breadcrumbs aria-label="breadcrumb" sx={{ color: 'white' }}>
-                    {location !== '/' && <Link to='/'>
-                        <Typography className={`${location === '/' && 'text-indigo-400'} hover:bg-slate-800`}>Home</Typography>
-                    </Link>}
-                    {(location.match(boardPagePattern) || location.match(projectsPagePattern) || location.match(projectPagePattern)) && <Link to='/projects'>
-                        <Typography className={`${location.match(projectsPagePattern) && 'text-indigo-400'} hover:bg-slate-800`}>Projects</Typography>
-                    </Link>}
-                    {location.match(projectPagePattern) && <Link to={`/project/${projectId}`}>
-                        <Typography className={`${location.match(projectPagePattern) && !location.match(boardPagePattern) && 'text-indigo-400'} hover:bg-slate-800`}>{projects[projectId]?.name}</Typography>
-                    </Link>}
-                    {location.match(boardPagePattern) && <Link to={`/project/${projectId}/board/${boardId}`}>
-                        <Typography className={`${location.match(boardPagePattern) && 'text-indigo-400'} hover:bg-slate-800`}>{projects[projectId]?.boards[boardId]?.title}</Typography>
-                    </Link>}
+                    {location !== '/' && (
+                        <Link to='/'>
+                            <Typography
+                                className={`hover:bg-slate-800 ${location === '/' &&
+                                    'text-indigo-400 cursor-default hover:bg-transparent'
+                                    } p-1 rounded-lg`
+                                }>
+                                Home
+                            </Typography>
+                        </Link>
+                    )}
+                    {(location.match(boardPagePattern) || location.match(projectsPagePattern) || location.match(projectPagePattern)) && (
+                        <Tooltip title='View All Projects' placement='top' arrow>
+                            <Link to='/projects'>
+                                <Typography
+                                    className={`hover:bg-slate-800 ${location.match(projectsPagePattern) &&
+                                        'text-indigo-400 cursor-default hover:bg-transparent'
+                                        } p-1 rounded-lg`
+                                    }
+                                >
+                                    Projects
+                                </Typography>
+                            </Link>
+                        </Tooltip>
+                    )}
+                    {location.match(projectPagePattern) && (
+                        <Tooltip title={`${projects[projectId]?.name} Dashboard`} placement='top' arrow>
+                            <Link to={`/project/${projectId}`}>
+                                <Typography
+                                    className={`hover:bg-slate-800 ${location.match(projectPagePattern)
+                                        && !location.match(boardPagePattern) && 'text-indigo-400 cursor-default hover:bg-transparent'
+                                        } p-1 rounded-lg`
+                                    }
+                                >{projects[projectId]?.name}
+                                </Typography>
+                            </Link>
+                        </Tooltip>
+                    )}
+                    {location.match(boardPagePattern) && (
+                        <Tooltip title="Board" placement='top' arrow>
+                            <Link to={`/project/${projectId}/board/${boardId}`}>
+                                <Typography
+                                    className={`hover:bg-slate-800 ${location.match(boardPagePattern)
+                                        && 'text-indigo-400 cursor-default hover:bg-transparent'
+                                        } p-1 rounded-lg`
+                                    }>
+                                    {projects[projectId]?.boards[boardId]?.title}
+                                </Typography>
+                            </Link>
+                        </Tooltip>
+                    )}
                 </Breadcrumbs>
             </div>
             {children && children}
